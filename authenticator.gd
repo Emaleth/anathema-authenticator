d70245e4e1f18dfc8cf95535f5b9e5cfec2cdcb5
Emaleth <Emaleth@protonmail.com>
2025-01-07 00:56:29 +0100

    update

--- authenticator.gd
+++ authenticator.gd
@@ -24,3 +24,26 @@ func _Peer_Connected(gateway_id):
 
 func _Peer_Disconnected(gateway_id):
 	print("Gateway " + str(gateway_id) + " Disconnected")
+
+
+@rpc("any_peer", "reliable")
+func gateway_to_authenticator_authenticate_player(player_id, _username, _password):
+	print("auth request recived")
+	var gateway_id = multiplayer.get_remote_sender_id()
+	var result
+	print("starting auth")
+	if not NewScript.player_ids.has(_username):
+		print("user not recognized")
+		result = false
+	elif not NewScript.player_ids[_username] == _password:
+		print("incorrect password")
+		result = false
+	else:
+		print("succesful auth")
+		result = true
+	print("auth result sent to gatewa")
+	authenticator_to_gateway_authenticate_player(gateway_id, result, player_id)
+
+@rpc("reliable")
+func authenticator_to_gateway_authenticate_player(gateway_id, result, player_id):
+	multiplayer.rpc(gateway_id, self, "authenticator_to_gateway_authenticate_player", [result, player_id])
---
+++ new_script.gd
@@ -0,0 +1,6 @@
+extends Node
+
+
+var player_ids = {
+	"123" : "qwe"
+}
